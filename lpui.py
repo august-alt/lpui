9e3311e8f5ca4ea8d7ffac175bf7aa2feb7f6d96
august-alt <77973983+august-alt@users.noreply.github.com>
2022-06-08 05:02:45 +0400

    feat: add python wrapper script

---
+++ lpui.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+import re
+import subprocess
+
+LOCAL_POLICY_DIR = "/var/cahce/local/policy"
+
+gpuiExecutable = '/usr/bin/gpui-main'
+gpuiParameters = ['--path', LOCAL_POLICY_DIR]
+
+wgetPopen = subprocess.Popen([gpuiExecutable] + gpuiParameters,
+                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+wgetPopen.stdout.close()
+wgetPopen.wait()

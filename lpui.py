77f3d3ffc4871ec473e886109d60721056f5dedb
august-alt <77973983+august-alt@users.noreply.github.com>
2022-06-09 11:03:24 +0400

    feat: complete refactoring of utility

--- lpui.py
+++ lpui.py
@@ -3,13 +3,15 @@
 import re
 import subprocess
 
-LOCAL_POLICY_DIR = "/var/cahce/local/policy"
+LOCAL_POLICY_DIR = '/var/cache/gpupdate/local_admin_policy'
 
 gpuiExecutable = '/usr/bin/gpui-main'
-gpuiParameters = ['--path', LOCAL_POLICY_DIR]
+gpuiParameters = ['-p', LOCAL_POLICY_DIR]
 
-wgetPopen = subprocess.Popen([gpuiExecutable] + gpuiParameters,
-                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+gpuiProcess = subprocess.call([gpuiExecutable] + gpuiParameters,
+                              stdin=subprocess.PIPE,
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.STDOUT)
 
-wgetPopen.stdout.close()
-wgetPopen.wait()
+gpuiProcess.stdout.close()
+gpuiProcess.wait()
